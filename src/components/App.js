d2c0a6b2a0340f5b720ba9edad37c69c3e77cb60
Yachareni, Krishnakanth <Krishnakanth.Yachareni@mastercard.com>
2019-06-14 20:41:12 +0530

    moment and react-icons packges has used

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,35 @@
-import React, { Component } from 'react';
-import '../css/App.css';
+import React, { Component } from "react";
+import "../css/App.css";
 
-import AddAppointments from './AddAppointments';
-import ListAppointments from './ListAppointments';
-import SearchAppointments from './SearchAppointments';
+import AddAppointments from "./AddAppointments";
+import ListAppointments from "./ListAppointments";
+import SearchAppointments from "./SearchAppointments";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      myAppointments: []
+      myAppointments: [],
+      lastIndex: 0
     };
   }
 
   // React Life cycle method
-componentDidMount() {
-  fetch('./data.json')
-  .then(response => response.json())
-  .then(result => {
-    const apts =  result.map(item => {
-        return item;
-    });
-   this.setState({
-      myAppointments: apts
-    }); 
-  });
-}
-  
+  componentDidMount() {
+    fetch("./data.json")
+      .then(response => response.json())
+      .then(result => {
+        const apts = result.map(item => {
+          item.aptId = this.state.lastIndex;
+          this.setState({ lastIndex: this.state.lastIndex + 1 });
+          return item;
+        });
+        this.setState({
+          myAppointments: apts
+        });
+      });
+  }
+
   render() {
     return (
       <main className="page bg-white" id="petratings">
@@ -34,9 +37,9 @@ componentDidMount() {
           <div className="row">
             <div className="col-md-12 bg-white">
               <div className="container">
-                <AddAppointments/>
-                <ListAppointments appointments={this.state.myAppointments}/>
-                <SearchAppointments/>
+                <AddAppointments />
+                <ListAppointments appointments={this.state.myAppointments} />
+                <SearchAppointments />
               </div>
             </div>
           </div>
