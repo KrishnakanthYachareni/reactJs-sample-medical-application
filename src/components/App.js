ab5c82ee5dd81ce4939d801f4976c34135968960
Yachareni, Krishnakanth <Krishnakanth.Yachareni@mastercard.com>
2019-06-14 20:12:06 +0530

    updated react code

---
+++ src/components/App.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import '../css/App.css';
+
+import AddAppointments from './AddAppointments';
+import ListAppointments from './ListAppointments';
+import SearchAppointments from './SearchAppointments';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      myAppointments: []
+    };
+  }
+
+  // React Life cycle method
+componentDidMount() {
+  fetch('./data.json')
+  .then(response => response.json())
+  .then(result => {
+    const apts =  result.map(item => {
+        return item;
+    });
+   this.setState({
+      myAppointments: apts
+    }); 
+  });
+}
+  
+  render() {
+    return (
+      <main className="page bg-white" id="petratings">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12 bg-white">
+              <div className="container">
+                <AddAppointments/>
+                <ListAppointments appointments={this.state.myAppointments}/>
+                <SearchAppointments/>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+    );
+  }
+}
+
+export default App;
