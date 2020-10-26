bf0701510295e3304bc49211e8ebce5a03c172c0
KrishnakanthYachareni <yacharenikrishnakanth@gmail.com>
2020-10-26 23:55:06 +0530

    Updates

---
+++ precache-manifest.2fc5c456a120135fd22e8aa66394ff10.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5db92e9b97f218af1825626a48e5e189",
+    "url": "/reactJs-sample-medical-application/index.html"
+  },
+  {
+    "revision": "7f0fb4b904dba836fa91",
+    "url": "/reactJs-sample-medical-application/static/css/2.6414bb7b.chunk.css"
+  },
+  {
+    "revision": "ed00237dd5fabf2cb1f0",
+    "url": "/reactJs-sample-medical-application/static/css/main.dcd4d52f.chunk.css"
+  },
+  {
+    "revision": "7f0fb4b904dba836fa91",
+    "url": "/reactJs-sample-medical-application/static/js/2.9a912f4b.chunk.js"
+  },
+  {
+    "revision": "ed00237dd5fabf2cb1f0",
+    "url": "/reactJs-sample-medical-application/static/js/main.cfa078c3.chunk.js"
+  },
+  {
+    "revision": "7b461fca3cf60c0e9f52",
+    "url": "/reactJs-sample-medical-application/static/js/runtime~main.49fdad4c.js"
+  }
+]);
